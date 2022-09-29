f3b22535eae418f50f787339d07786d373f2d49a
Silverius Daniel Wijono <swijono@quadrant.io>
2022-09-29 22:11:46 +0700

    create the feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Silverius Daniel Wijono on 29/09/22.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
