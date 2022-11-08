772151ec90838fe880a841395220f31639c6446d
Silverius Daniel Wijono <swijono@quadrant.io>
2022-11-08 21:38:39 +0700

    validate cache command does not delete cache when cache is already empty

--- EssentialFeedTests/Feed Cache/LocalFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LocalFeedFromCacheUseCaseTests.swift
@@ -83,7 +83,7 @@ class LocalFeedFromCacheUseCaseTests: XCTestCase {
     XCTAssertEqual(store.receivedMessages, [.retrieve])
   }
   
-  func test_load_doesNotDeletesCacheOnEmptyCache() {
+  func test_load_hasNoSideEffectsOnEmptyCache() {
     let (sut, store) = makeSUT()
     
     sut.load { _ in }
