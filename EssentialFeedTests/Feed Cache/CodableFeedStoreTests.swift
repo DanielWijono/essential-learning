0acd3e51b9c29224bf591b3a787b44cb0906568c
Silverius Daniel Wijono <swijono@quadrant.io>
2022-12-13 21:10:58 +0700

    Create FeedStoreSpecs that must be implemented by any FeedStore t… …est implementation to guarantee it meets spec

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -9,40 +9,11 @@ import XCTest
 import EssentialFeed
 import Foundation
 
-protocol FeedStoreSpecs {
-  func test_retrieve_deliversOnEmptyCache()
-  func test_retrieve_hasNoSideEffectsOnEmptyCache()
-  func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-  func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
-  
-  func test_insert_deliversNoErrorOnEmptyCache()
-  func test_insert_deliversNoErrorOnNonEmptyCache()
-  func test_insert_overridesPreviouslyInsertedCacheValues()
-  
-  func test_delete_deliversNoErrorOnEmptyCache()
-  func test_delete_hasNoSideEffectsOnEmptyCache()
-  func test_delete_deliversNoErrorOnNonEmptyCache()
-  func test_delete_emptiesPreviouslyInsertedCache()
-  
-  func test_storeSideEffects_runSerially()
-}
 
-protocol FailableRetrieveFeedStoreSpecs {
-  func test_retrieve_deliversFailureOnRetrievalError()
-  func test_retrieve_hasNoSideEffectsOnFailure()
-}
 
-protocol FailableInsertFeedStoreSpecs {
-  func test_insert_deliversErrorOnInsertionError()
-  func test_insert_hasNoSideEffectsOnInsertionError()
-}
+typealias FailableFeedStore = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
 
-protocol FailableDeleteFeedStoreSpecs {
-  func test_delete_deliversErrorOnDeletionError()
-  func test_delete_hasNoSideEffectsOnDeletionError()
-}
-
-class CodableFeedStoreTests: XCTestCase {
+class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
   
   override func setUp() {
     super.setUp()
@@ -102,6 +73,17 @@ class CodableFeedStoreTests: XCTestCase {
     try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
     
     expect(sut, toRetrieveTwice: .failure(anyNSError()))
+  }
+  
+  func test_insert_hasNoSideEffectsOnInsertionError() {
+    let invalidStoreURL = URL(string: "invalid://store-url")!
+    let sut = makeSUT(storeURL: invalidStoreURL)
+    let feed = uniqueImageFeed().local
+    let timestamp = Date()
+    
+    insert((feed, timestamp), to: sut)
+    
+    expect(sut, toRetrieve: .empty)
   }
   
   func test_insert_deliversNoErrorOnEmptyCache() {
