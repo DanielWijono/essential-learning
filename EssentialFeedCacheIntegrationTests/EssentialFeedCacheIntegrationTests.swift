1ffeedeac7fc4ec5bbe6027e0c0f8bbffd163f57
Silverius Daniel Wijono <swijono@quadrant.io>
2022-12-29 17:48:11 +0700

    Add EssentialFeedCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Silverius Daniel Wijono on 29/12/22.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+  
+}
