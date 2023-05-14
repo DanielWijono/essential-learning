b7f58ea86f0fd842c8693f55dccbd535c76c663f
Silverius Daniel Wijono <swijono@quadrant.io>
2023-05-14 12:32:03 +0700

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Silverius Daniel Wijono on 14/05/23.
+//
+
+struct FeedLoadingViewModel {
+   let isLoading: Bool
+ }
---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Silverius Daniel Wijono on 14/05/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+  let feed: [FeedImage]
+}
