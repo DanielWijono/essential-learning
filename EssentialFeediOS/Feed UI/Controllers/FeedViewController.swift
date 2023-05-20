9121e3324fea3303bcbf00e7a0c20c6ff37f8d22
Silverius Daniel Wijono <swijono@quadrant.io>
2023-05-20 09:33:24 +0700

    Merge FeedRefreshViewController with FeedViewController since the `UIRefreshControl` creation and configuration now lives in the Storyboard

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
-  @IBOutlet var refreshController: FeedRefreshViewController?
+protocol FeedViewControllerDelegate {
+  func didRequestFeedRefresh()
+}
+
+final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
+  var delegate: FeedViewControllerDelegate?
   
   private var imageLoader: FeedImageDataLoader?
   
@@ -22,7 +26,19 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     super.viewDidLoad()
     
     tableView.prefetchDataSource = self
-    refreshController?.refresh()
+    refresh()
+  }
+  
+  @IBAction private func refresh() {
+    delegate?.didRequestFeedRefresh()
+  }
+  
+  func display(_ viewModel: FeedLoadingViewModel) {
+    if viewModel.isLoading {
+      refreshControl?.beginRefreshing()
+    } else {
+      refreshControl?.endRefreshing()
+    }
   }
   
   public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
