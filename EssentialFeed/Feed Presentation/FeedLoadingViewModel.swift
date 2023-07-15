ebd3d1e037b30f32a10114d0342665f94bbebd79
Silverius Daniel Wijono <swijono@quadrant.io>
2023-07-15 08:43:45 +0800

    Move FeedLoadingViewModel to separate file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Silverius Daniel Wijono on 15/07/23.
+//
+
+public struct FeedLoadingViewModel {
+  public let isLoading: Bool
+}
