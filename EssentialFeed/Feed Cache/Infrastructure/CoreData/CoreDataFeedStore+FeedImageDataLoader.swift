c047d2307879853a9c1418c6c0e707c7c9a18ccb
Silverius Daniel Wijono <swijono@quadrant.io>
2023-08-08 15:54:06 +0700

    Move CoreDataFeedStore implementation of the FeedImageDataStore protocol to production

---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataFeedStore+FeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Silverius Daniel Wijono on 08/08/23.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+  
+  public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+  }
+  
+  public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+    completion(.success(.none))
+  }
+  
+}
