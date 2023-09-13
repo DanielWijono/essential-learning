9ab161d67c7260130c684709e779f9575a71245c
Silverius Daniel Wijono <swijono@quadrant.io>
2023-09-14 00:15:09 +0700

    Rename and move ResourceErrorView and ViewModel to Shared module

---
+++ EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by Silverius Daniel Wijono on 14/09/23.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+  func display(_ viewModel: ResourceErrorViewModel)
+}
