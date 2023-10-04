e58cd548ac065740ba533938899917b6fe1cde0f
Silverius Daniel Wijono <swijono@quadrant.io>
2023-10-04 23:59:56 +0700

    add snapshots for essential feed iOS tests

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -5,13 +5,12 @@
 //  Created by Silverius Daniel Wijono on 21/08/23.
 //
 
-import Combine
 import UIKit
 import CoreData
+import Combine
 import EssentialFeed
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  
   var window: UIWindow?
   
   private lazy var httpClient: HTTPClient = {
@@ -60,7 +59,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
   
   private func showComments(for image: FeedImage) {
-    let url = baseURL.appendingPathComponent("/v1/image/\(image.id)/comments")
+    let url = ImageCommentsEndpoint.get(image.id).url(baseURL: baseURL)
     let comments = CommentsUIComposer.commentsComposedWith(commentsLoader: makeRemoteCommentsLoader(url: url))
     navigationController.pushViewController(comments, animated: true)
   }
@@ -75,10 +74,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
   
   private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<[FeedImage], Error> {
-    let remoteURL = baseURL.appendingPathComponent("/v1/feed")
+    let url = FeedEndpoint.get.url(baseURL: baseURL)
     
     return httpClient
-      .getPublisher(url: remoteURL)
+      .getPublisher(url: url)
       .tryMap(FeedItemsMapper.map)
       .caching(to: localFeedLoader)
       .fallback(to: localFeedLoader.loadPublisher)
